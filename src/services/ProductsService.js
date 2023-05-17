59484f6e4cbb32d3e02143d2e3d265d09e73fa87
5david-hub5 <bane01@mail.ru>
2023-05-17 19:38:58 +0300

    create services

---
+++ src/services/ProductsService.js
@@ -0,0 +1,35 @@
+import $api from "../http";
+
+export default class ProductsService {
+  static async addToFavorite(id, username) {
+    return await $api.post(`/products/addtofavorite`, {
+      id, username
+    });
+  }
+
+  static async removeFromFavorite(id, username) {
+    return await $api.post(`/products/deletefromfavorite`, {
+      id, username
+    })
+  }
+
+  static async getAllProducts() {
+    return await $api.get(`/products/getproductlist`);
+  }
+
+  static async getProductById(id) {
+    return await $api.get(`/products/getproduct?id=${id}`);
+  }
+
+  static async getProductsByCategory(category_id) {
+    return await $api.get(`/products/getproductlistwithcategory?id=${category_id}`);
+  }
+
+  static async getAllCategories() {
+    return await $api.get(`/productscategory/getproductscategorys`);
+  }
+
+  static async getProductComments(id) {
+    return await $api.get(`/products/getcommentslist?id=${id}`);
+  }
+}
