0f39202c40a0b92d3525420aab17b4713be372be
5david-hub5 <bane01@mail.ru>
2023-05-19 20:38:53 +0300

    add searchbar on products page

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://158.160.4.158:30/";
+export const API_URL = "http://51.250.30.255:30/";
 
 const $api = axios.create({
   baseURL: API_URL
